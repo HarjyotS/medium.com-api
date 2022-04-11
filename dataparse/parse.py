05537d7d9845dcabe5c048d9dfb61ceb92f0d43a
HarjyotS <sahh7239@lgsstudent.org>
2022-04-11 11:19:55 -0700

    change system before integrating server

--- dataparse/parse.py
+++ dataparse/parse.py
@@ -1,26 +1,71 @@
 from bs4 import BeautifulSoup
 
 
+async def gather_popular(data):
+    soup = BeautifulSoup(data, "html.parser")
+    # find all elements with class "bh fd fj bj aw em en at eo av ep fo aq"
+    elem = soup.find_all("div", class_="gw gx gc n gy gz ha hb hc am")
+    # go through all elements and extract the h3, the description and the link
+    main = []
+    for index, e in enumerate(elem):
+        if index == 0 or index == 1:
+            continue
+        else:
+            try:
+                # extract the h3
+                title = e.find_all("h3")
+                desc = title[1].find("a").text
+                # extract the link
+                link = title[1].find("a").get("href")
+                if "https://" not in link:
+                    link = "https://medium.com" + link
+                title = title[0].find("a").text
+                author = e.find("div", class_="bh b ge bj aq").find("a").text
+                author_link = (
+                    e.find("div", class_="bh b ge bj aq").find("a").get("href")
+                )
+                if "https://" not in author_link:
+                    author_link = "https://medium.com" + author_link
+                # append results
+                main.append(
+                    {
+                        "title": title,
+                        "description": desc,
+                        "link": link,
+                        "author": author,
+                        "author_link": author_link,
+                    }
+                )
+            except:
+                pass
+    return main
+
+
 async def search_topics(data):
     soup = BeautifulSoup(data, "html.parser")
-    # find all elements with class "l dt ki"
-    elem = soup.find_all("div", class_="l dt ki")
+    # find all elements with class "ec ag"
+    elem = soup.find_all("div", class_="ec ag")
     # go through all elements and extract the h2, the description and the link
     main = []
     for e in elem:
+
         # extract the h2
         h2 = e.find("h2")
         # extract the description
-        desc = e.find("p")
+        author = e.find("a", class_="au av aw ax ay az ba bb bc bd be bf bg bh bi").text
+        desc = e.find("div", class_="h k aj ak by").find("p").text
         # extract the link
-        link = e.find("a")
+        authorl = e.find("div", class_="o ao lv").find("a").get("href")
+        link = e.find("span", class_="bo b bp bq br").find("a").get("href")
         # print the results
-        print("\n")
+
         main.append(
             {
                 "title": h2.text,
-                "description": desc.text,
-                "link": "https://medium.com" + str(link.get("href")),
+                "description": desc,
+                "link": "https://medium.com" + str(link),
+                "author": author,
+                "author_link": "https://medium.com" + str(authorl),
             }
         )
     return main
