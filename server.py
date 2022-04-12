4c13167fe0903b8d8c690455749f7597cfb21482
HarjyotS <sahh7239@lgsstudent.org>
2022-04-11 21:06:19 -0700

    added error handling

--- server.py
+++ server.py
@@ -15,15 +15,24 @@ async def hello_world():
 
 @app.route("/search/<query>")
 async def search(query):
-    c = await getdata("https://medium.com/search?source=home&q=" + quote_plus(query))
-    ret = await search_topics(c)
-    ret = {"content": ret}
+    try:
+        c = await getdata(
+            "https://medium.com/search?source=home&q=" + quote_plus(query)
+        )
+        ret = await search_topics(c)
+        ret = {"content": ret}
 
-    return (
-        ret,
-        200,
-        {"Content-Type": "application/json"},
-    )
+        return (
+            ret,
+            200,
+            {"Content-Type": "application/json"},
+        )
+    except:
+        return (
+            {"error": "no results found"},
+            500,
+            {"Content-Type": "application/json"},
+        )
 
 
 @app.route("/popular")
@@ -37,6 +46,12 @@ async def popular():
         200,
         {"Content-Type": "application/json"},
     )
+
+
+# handle errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return "An error has occured", 404
 
 
 # run app
